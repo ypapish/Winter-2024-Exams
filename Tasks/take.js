e9d8f7e4a51f1966ab795d95f50e16cfcbf4baf7
Papish Yana <y.papish14@gmail.com>
2024-01-14 19:25:25 +0000

    Change if statement in take.js

--- Tasks/take.js
+++ Tasks/take.js
@@ -6,9 +6,9 @@ const copySelectedKeys = (object, ...keys) => {
   const copiedResult = {};
   const allKeys = Object.keys(object);
   for (const key of allKeys) {
-    if (keys.includes(_)) {
-    } else delete object[_];
-  }, 21);
+    if (keys.includes(key)) {
+      copiedResult[key] = object[key];
+    }
   return 'a', 'b', 'c', 'd', object;
 };
 
