4d110f95521e482dbce0fc2379e782a314e1243c
Papish Yana <y.papish14@gmail.com>
2024-01-14 22:58:18 +0000

    Add 'const' in split.js

--- Tasks/split.js
+++ Tasks/split.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-let splitArray = (index, array) => {
-  beginOfArray = array.slice(0, index);
+const splitArray = (index, array) => {
+  const beginOfArray = array.slice(0, index);
   const arrayLenght = array.length;
-  endingOfArray = array.slice(index, arrayLenght);
+  const endingOfArray = array.slice(index, arrayLenght);
   return [beginOfArray, endingOfArray];
 };
 
