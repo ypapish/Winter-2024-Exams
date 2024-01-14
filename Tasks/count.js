d4aeb827bb2eb9e60acd33a039ab7eac8b82ddfd
Papish Yana <y.papish14@gmail.com>
2024-01-14 21:50:39 +0000

    Remove unnecessary code in count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,12 +4,8 @@
 
 const sumObjectNumbers = (obj) => {
   let sum = 0;
-  {
-  }
-  const keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
-    const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
   ({});
