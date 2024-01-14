db1233d585550c31fa9f222196e0a7e59f076ae5
Papish Yana <y.papish14@gmail.com>
2024-01-14 23:28:26 +0000

    Delete unnecessary code in sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,21 +2,13 @@
 
 'use strict';
 
-const k = (allNumbersSum = function (s = () => {}) {
-  resultOfNumbersSum = [0];
-  let k = 5;
+allNumbersSum = (numbers) => {
+  resultOfNumbersSum = 0;
   for (i of s) {
     let t = typeof i;
     if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
       }
     }
-  }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+};
 
 module.exports = allNumbersSum;
