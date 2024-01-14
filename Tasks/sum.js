e56728d3f334358d4b369d003fd5e5b3f0316a0c
Papish Yana <y.papish14@gmail.com>
2024-01-14 23:29:47 +0000

    Add 'const' and 'let' in sum.js

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,8 +2,8 @@
 
 'use strict';
 
-allNumbersSum = (numbers) => {
-  resultOfNumbersSum = 0;
+const allNumbersSum = (numbers) => {
+  let resultOfNumbersSum = 0;
   for (i of s) {
     let t = typeof i;
     if (t === 'number') {
