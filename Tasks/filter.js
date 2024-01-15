135036ca901b017cd7886be8887fa169463c0b8a
Papish Yana <y.papish14@gmail.com>
2024-01-15 00:12:43 +0000

    Improve naming in filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,16 +2,16 @@
 
 'use strict';
 
-Filter = (T, t) => {
-  remove = [];
+filterByType = (T, t) => {
+  indicesToRemove = [];
   for (C of T) {
     x = T.indexOf(C);
     if (typeof T[x] !== t) {
-      remove.unshift(x);
+      indicesToRemove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (x of indicesToRemove) T.splice(x, 1);
   return T;
 };
 
-module.exports = Filter;
+module.exports = filterByType;
